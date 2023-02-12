122a3f9a6bfd7fcce3178d7d8b2eb089af5a05e6
Nihat Halici <nihat.halici@gmail.com>
2023-02-12 15:09:45 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=%5d5(o%0q)v08e_gj78f!&i11l6l3k-!%h64+0p+*c!n(uzz0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nihathalici.pythonanywhere.com']
 
 
 # Application definition
